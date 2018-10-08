32e26d51aadc259ebe79950a1373345becf3a066
Joel Bourbonnais <joelbourbon@gmail.com>
2018-10-07 21:01:35 -0400

    Added segment management

--- firmware/lib/WebDriver/WebDriver.h
+++ firmware/lib/WebDriver/WebDriver.h
@@ -6,6 +6,7 @@
 
 struct StripState
 {
+  unsigned int index;
   unsigned int onOff;
   unsigned int hue;
   unsigned int saturation;
@@ -27,69 +28,79 @@ public:
 
   void Init(QueueHandle_t stripCommandQueue, QueueHandle_t stripConfigQueue)
   {
-    _stripState.onOff = 0;
-    _stripState.brightness = 0;
-    _stripState.hue = 0;
-    _stripState.saturation = 100;
-
     _stripCommandQueue = stripCommandQueue;
     _stripConfigQueue = stripConfigQueue;
+
+    for (unsigned int i = 0; i < 10; i++)
+    {
+      _stripState[i].onOff = 1;
+      _stripState[i].brightness = 100;
+      _stripState[i].saturation = 100;
+      _stripState[i].hue = 225;
+      _stripState[i].index = i;
+
+      xQueueSend(_stripCommandQueue, &(_stripState[i]), portMAX_DELAY);
+    }
+
     _webServer = new AsyncWebServer(80);
 
     _webServer->on("/status", [&](AsyncWebServerRequest *request)
     {
-      request->send(200, "text/plain", String(_stripState.onOff));
+      request->send(200, "text/plain", String(_stripState[GetIndex(request)].onOff));
     });
 
     _webServer->on("/on", [&](AsyncWebServerRequest *request)
     {
       Serial.println("Received a On command.");
-      _stripState.onOff = 1;
-      xQueueSend(_stripCommandQueue, &_stripState, portMAX_DELAY);
-      request->send(200, "text/plain", "Strip was turned on.");
+      int segmentIndex = GetIndex(request);
+      _stripState[segmentIndex].onOff = 1;
+      xQueueSend(_stripCommandQueue, &_stripState[GetIndex(request)], portMAX_DELAY);
+      request->send(200, "text/plain", "Strip segment " + String(segmentIndex) + " was turned on.");
     });
 
     _webServer->on("/off", [&](AsyncWebServerRequest *request)
     {
       Serial.println("Received a Off command.");
-      _stripState.onOff = 0;
-      xQueueSend(_stripCommandQueue, &_stripState, portMAX_DELAY);
-      request->send(200, "text/plain", "Strip was turned off.");
+      int segmentIndex = GetIndex(request);
+      _stripState[segmentIndex].onOff = 0;
+      xQueueSend(_stripCommandQueue, &_stripState[GetIndex(request)], portMAX_DELAY);
+
+      request->send(200, "text/plain", "Strip segment " + String(segmentIndex) + " was turned off.");
     });
 
     _webServer->on("/hue", [&](AsyncWebServerRequest *request)
     {
-      request->send(200, "text/plain", String(_stripState.hue));
+      request->send(200, "text/plain", String(_stripState[GetIndex(request)].hue));
     });
 
     _webServer->on("/saturation", [&](AsyncWebServerRequest *request)
     {
-      request->send(200, "text/plain", String(_stripState.saturation));
+      request->send(200, "text/plain", String(_stripState[GetIndex(request)].saturation));
     });
 
     _webServer->on("/brightness", [&](AsyncWebServerRequest *request)
     {
-      request->send(200, "text/plain", String(_stripState.brightness));
+      request->send(200, "text/plain", String(_stripState[GetIndex(request)].brightness));
     });
 
     _webServer->on("/set", [&](AsyncWebServerRequest *request)
     {      
       if (request->hasParam("h"))
       {
-        _stripState.hue = request->getParam("h")->value().toInt();
+        _stripState[GetIndex(request)].hue = request->getParam("h")->value().toInt();
       }
 
       if (request->hasParam("s"))
       {
-        _stripState.saturation = request->getParam("s")->value().toInt();
+        _stripState[GetIndex(request)].saturation = request->getParam("s")->value().toInt();
       }
 
       if (request->hasParam("b"))
       {
-        _stripState.brightness = request->getParam("b")->value().toInt();
+        _stripState[GetIndex(request)].brightness = request->getParam("b")->value().toInt();
       }      
       
-      xQueueSend(_stripCommandQueue, &_stripState, portMAX_DELAY);
+      xQueueSend(_stripCommandQueue, &_stripState[GetIndex(request)], portMAX_DELAY);
       request->send(200);
     });
 
@@ -119,11 +130,23 @@ public:
     _webServer->begin();
   }
 
+  unsigned int GetIndex(AsyncWebServerRequest *request)
+  {
+    if (request->hasParam("index"))
+    {
+      return request->getParam("index")->value().toInt();
+    }
+    else
+    {
+      return 0;
+    }
+  }
+
 protected:
   AsyncWebServer *_webServer;
   unsigned int _id;
   QueueHandle_t _stripCommandQueue;
   QueueHandle_t _stripConfigQueue;
   unsigned int _onOffState = 0;
-  StripState _stripState;
+  StripState _stripState[10];
 };
